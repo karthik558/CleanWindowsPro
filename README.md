06dfc8c247d082bcb7b939c0d878cfc5ab2f9c5f
KARTHIK LAL <karthik.lal558@gmail.com>
2022-06-10 16:02:26 +0530

    win-script: initial push wrapper-script to the repository
    Signed-off-by: KARTHIK LAL <karthik.lal558@gmail.com>

---
+++ README.md
@@ -0,0 +1,25 @@
+## <b> ABOUT THIS PROJECT </b>
+
+* This is a simple windows clean-up project. It is intended to be used as a tool to clean up your windows environment.
+* It is not intended to be a replacement for a full-functional tool.
+
+## <b> HOW TO USE THIS PROJECT </b>
+
+* ` git clone https://github.com/karthik558/clean-windows.git `
+* ` cd clean-windows `
+* ` ./clean-win.bat `
+
+## <b> INFORMATION ABOUT THIS PROJECT </b>
+
+* This script is just a simple wrapper around the `clean-win.bat` script.
+* Every commands on this script can be executed by just typing the command name. (i made it easily that's it.)
+* TOOLS ARE PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND AND THE AUTHOR IS NOT RESPONSIBLE FOR ANY DAMAGE CAUSED BY THE USE OF THESE TOOLS
+* USE AT YOUR OWN RISK
+
+## <b> CONTRIBUTION AND LISCENCE </b>
+
+* If you want to contribute to this project, then follow github-basic-contribution rules. (inlude signoff and commit message) (https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/writing-a-pull-request)
+* LICESNSE: GNU GENERAL PUBLIC LICENSE V3.0
+* OPENSOURCE SOFTWARE LICENSE AGREEMENT FOR WINDOWS DISTRIBUTION SYSTEM (https://www.gnu.org/licenses/gpl-3.0.en.html)
+* Copying and distribution of this file, with or without modification, are permitted in any medium without royalty provided the copyright notice and this notice are preserved.
+* Copyright (c) 2022, KARTHIK LAL. All rights reserved.
---
+++ clean-win.bat
@@ -0,0 +1,95 @@
+@echo off
+color 0a &:: hacker color (just kidding)
+
+title KARTHIK-V1.0 &:: title is needed cuz of the way the console is displayed
+echo YOU ARE USING KARTHIK-V1.0-BETA WINDOWS-CLEAN SCRIPT &:: Ofc we need to mention the tool before starting the script
+timeout /t 3 &:: timeout is used to make the script wait for 4 seconds
+
+echo BYE BYE EXPLORER.exe &:: lets make it look like a real program :)
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+taskkill /f /im explorer.exe &:: kill the explorer.exe
+
+echo Removing .log files &:: delete all the .log files
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del %windir%\KB*.log /f /q  &:: delete all the files with KB in the name
+
+echo Removing System Tempory Files &:: delete all the files in the system tempory folder
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /s /q %systemdrive%\*.tmp &:: delete all the .tmp files in the system drive
+
+echo Removing *_mp files &:: delete all the files with _mp in the name
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /s /q %systemdrive%\*._mp &:: delete all the ._mp files in the system drive
+
+echo Removing *.log files &:: delete all the files with .log in the name
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /s /q %systemdrive%\*.log &:: delete all the .log files in the system drive
+
+echo Remvoing *.gid files &:: delete all the files with .gid in the name
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /s /q %systemdrive%\*.gid &:: delete all the .gid files in the system drive
+
+echo Removing .chk files &:: delete all the files with .chk in the name
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /s /q %systemdrive%\*.chk &:: delete all the .chk files in the system drive
+
+echo Removing .old files &:: delete all the files with .old in the name
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /s /q %systemdrive%\*.old &:: delete all the .old files in the system drive
+
+echo Removing recycled files &:: delete all the files in the recycle bin
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /s /q %systemdrive%\recycled\*.* &:: delete all the files in the recycle bin
+
+echo Removing *.bak files &:: delete all the files with .bak in the name
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /s /q %windir%\*.bak &:: delete all the .bak files in the system drive
+
+echo Removing prefetch files &:: delete all the files with .pf in the name
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /s /q %windir%\prefetch\*.* &:: delete all the .pf files in the system drive
+
+echo Removing temp files &:: delete all the files with .tmp in the name
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+rd /s /q %windir%\temp & md %windir%\temp &:: delete all the .tmp files in the system drive
+
+echo Removing default stored cookies &:: delete all the files with .tmp in the name
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /q %userprofile%\cookies\*.* &:: delete all the .tmp files in the system drive
+
+echo Removing temporary_internet_files &:: delete all the files with .tmp in the name
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /s /q "%userprofile%\local settings\temporary internet files\*.*" &:: delete all the .tmp files in the system drive
+
+echo Removing local_settings__temp files &:: delete all the files with .tmp in the name
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /s /q "%userprofile%\local settings\temp\*.*" &:: delete all the .tmp files in the system drive
+
+echo Removing recent files &:: delete all the files with .tmp in the name
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+del /f /s /q "%userprofile%\recent\*.*" &:: delete all the .tmp files in the system drive
+
+echo Scanning all protected system files, and replacing corrupted files
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+sfc /scannow &:: scan all the protected system files and fix if any are corrupted
+
+echo Print out directory_tree_list &:: print out the directory tree list
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+dir /s &:: check all directory_trees and print out the list (just for final view)
+
+explorer.exe &:: restart the explorer.exe which we already killed earlier
+
+echo Thank you for using KARTHIK-V1.0 &:: GREETINGS
+timeout /t 1 &:: timeout is used to make the script wait for 1 seconds
+
+&:: ================================================================
+&:: AUTHOR: KARTHIK LAL & :: EMAIL: karthik.lal558@gmail.com
+&:: DATE: 10-06-2022
+&:: WINDOWS CLEANER V1.0-BETA
+&:: TOOLS ARE PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND AND THE AUTHOR IS NOT RESPONSIBLE FOR ANY DAMAGE CAUSED BY THE USE OF THESE TOOLS
+&:: USE AT YOUR OWN RISK
+&:: LICESNSE: GNU GENERAL PUBLIC LICENSE V3.0
+&:: OPENSOURCE SOFTWARE LICENSE AGREEMENT FOR WINDOWS DISTRIBUTION SYSTEM (https://www.gnu.org/licenses/gpl-3.0.en.html)
+&:: Copying and distribution of this file, with or without modification, are permitted in any medium without royalty provided the copyright notice and this notice are preserved.
+&:: Copyright (c) 2022 KARTHIK LAL
+&:: ================================================================
